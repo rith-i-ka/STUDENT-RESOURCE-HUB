fb4b762022ce757b6244a8f9fdbc5d98bc5fd0ee
rith-i-ka <24256.cs@rmkcet.ac.in>
2026-03-19 15:35:11 +0530

    Added

--- main.py
+++ main.py
@@ -5,18 +5,30 @@ from utils.helper import save_to_json, print_data
 
 def main():
     keyword = input("Enter keyword: ")
+    print("\nSelect type:")
+    print("1. GitHub")
+    print("2. Academics")
+    print("3. Internships")
 
-    github_data = scrape_github(keyword)
-    acad_data = scrape_academics(keyword)
-    intern_data = scrape_internships(keyword)
+    choice = input("Enter choice (1/2/3): ")
 
-    save_to_json(github_data, "github.json")
-    save_to_json(acad_data, "academics.json")
-    save_to_json(intern_data, "internships.json")
+    if choice == "1":
+        data = scrape_github(keyword)
+        save_to_json(data, "github.json")
+        print_data("GitHub", data)
 
-    print_data("GitHub", github_data)
-    print_data("Academics", acad_data)
-    print_data("Internships", intern_data)
+    elif choice == "2":
+        data = scrape_academics(keyword)
+        save_to_json(data, "academics.json")
+        print_data("Academics", data)
+
+    elif choice == "3":
+        data = scrape_internships(keyword)
+        save_to_json(data, "internships.json")
+        print_data("Internships", data)
+
+    else:
+        print("Invalid choice ❌")
 
 if __name__ == "__main__":
     main()
