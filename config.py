c2df17e7ffcf5b5a6d8ec6058348ee4a2f1c3540
Sainiveta <sainivetas@gmail.com>
2026-03-19 21:51:09 +0530

    Success

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 HEADERS = {
-    "User-Agent": "Mozilla/5.0"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+    "Accept-Language": "en-US,en;q=0.9"
 }
-
 GITHUB_URL = "https://api.github.com/search/repositories?q="
 SCHOLAR_URL = "https://scholar.google.com/scholar?q="
 INTERNSHIP_URL = "https://internshala.com/internships/keywords-"
