8a1856a1688ec8767c1d672291d020ecd9854b45
Sainiveta <sainivetas@gmail.com>
2026-03-19 20:36:05 +0530

    Success

---
+++ frontend/script.js
@@ -0,0 +1,24 @@
+async function search(type) {
+    const keyword = document.getElementById("keyword").value;
+
+    const res = await fetch(`http://127.0.0.1:5000/search?q=${keyword}&type=${type}`);
+    const data = await res.json();
+
+    display(data.results);
+}
+
+function display(items) {
+    const container = document.getElementById("results");
+    container.innerHTML = "";
+
+    items.forEach(item => {
+        const div = document.createElement("div");
+        div.className = "card";
+
+        div.innerHTML = `
+            <pre>${JSON.stringify(item, null, 2)}</pre>
+        `;
+
+        container.appendChild(div);
+    });
+}
